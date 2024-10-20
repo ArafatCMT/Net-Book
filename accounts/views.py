f1dadd8cb489b7e0880ef11fee34e9f1f508eade
ArafatCMT <arafatemon6969@gmail.com>
2024-10-20 22:49:09 +0600

    login view update

--- accounts/views.py
+++ accounts/views.py
@@ -76,13 +76,13 @@ class LoginView(APIView):
 
             if user:
                 token, _ = Token.objects.get_or_create(user=user)
-                print('get',token)
-                print(_)
+                # print('get',token)
+                # print(_)
 
                 login(request, user)
                 account = Account.objects.get(user=user)
-                print(account.id)
-                print(account)
+                # print(account.id)
+                # print(account)
                 return Response({'token': token.key, 'account_id': account.id})
             else:
                 return Response({'error': "Invalid Credential"})
